1c244b0ec6fd072d4898d942a7dd81ad06ceb1a9
qky <qky1412@gmail.com>
2016-04-16 16:19:38 +0800

    增加了合作流程的板块显示

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,124 +1,126 @@
 /*
-	Escape Velocity by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+ Escape Velocity by HTML5 UP
+ html5up.net | @n33co
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ */
 
-(function($) {
+(function ($) {
 
-	skel
-		.breakpoints({
-			desktop: '(min-width: 737px)',
-			tablet: '(min-width: 737px) and (max-width: 1200px)',
-			mobile: '(max-width: 736px)'
-		})
-		.viewport({
-			breakpoints: {
-				tablet: {
-					width: 1080
-				}
-			}
-		});
+    skel
+        .breakpoints({
+            desktop: '(min-width: 737px)',
+            tablet: '(min-width: 737px) and (max-width: 1200px)',
+            mobile: '(max-width: 736px)'
+        })
+        .viewport({
+            breakpoints: {
+                tablet: {
+                    width: 1080
+                }
+            }
+        });
 
-	$(function() {
-		var	$window = $(window),
-			$body = $('body');
+    $(function () {
+        var $window = $(window),
+            $body = $('body');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+        $window.on('load', function () {
+            $body.removeClass('is-loading');
+        });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
+        // CSS polyfills (IE<9).
+        if (skel.vars.IEVersion < 9)
+            $(':last-child').addClass('last-child');
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
-		var elm = $('#nav');
-		var startPos = $(elm).offset().top;
-		$.event.add(window, "scroll", function() {
-			var p = $(window).scrollTop();
-			$(elm).css('position',((p) > startPos) ? 'fixed' : 'absolute');
-			$(elm).css('top',((p) > startPos) ? '0px' : '');
-		});
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				mode: 'fade',
-				noOpenerFade: true,
-				alignment: 'center',
-				detach: false
-			});
+        // Prioritize "important" elements on mobile.
+        skel.on('+mobile -mobile', function () {
+            $.prioritize(
+                '.important\\28 mobile\\29',
+                skel.breakpoint('mobile').active
+            );
+        });
+        var elm = $('#nav');
+        var startPos = $(elm).offset().top;
+        $.event.add(window, "scroll", function () {
+            var p = $(window).scrollTop();
+            $(elm).css('position', ((p) > startPos) ? 'fixed' : 'absolute');
+            $(elm).css('top', ((p) > startPos) ? '0px' : '');
+        });
+        // Dropdowns.
+        $('#nav > ul').dropotron({
+            mode: 'fade',
+            noOpenerFade: true,
+            alignment: 'center',
+            detach: false
+        });
 
-		// Off-Canvas Navigation.
+        // Off-Canvas Navigation.
 
-			// Title Bar.
-				$(
-					'<div id="titleBar">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
-					'</div>'
-				)
-					.appendTo($body);
+        // Title Bar.
+        $(
+            '<div id="titleBar">' +
+            '<a href="#navPanel" class="toggle"></a>' +
+            '<span class="title">' + $('#logo').html() + '</span>' +
+            '</div>'
+        )
+            .appendTo($body);
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<h3>在田信息技术有限公司</h3>' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
+        // Navigation Panel.
+        $(
+            '<div id="navPanel">' +
+            '<h3>在田信息技术有限公司</h3>' +
+            '<nav>' +
+            $('#nav').navList() +
+            '</nav>' +
+            '</div>'
+        )
+            .appendTo($body)
+            .panel({
+                delay: 500,
+                hideOnClick: true,
+                hideOnSwipe: true,
+                resetScroll: true,
+                resetForms: true,
+                side: 'left',
+                target: $body,
+                visibleClass: 'navPanel-visible'
+            });
 
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#titleBar, #navPanel, #page-wrapper')
-						.css('transition', 'none');
-				// Initialize WOW.js Scrolling Animations
-				new WOW().init();
-		$('.collapse').on('show.bs.collapse', function (e) {
-			$('.collapse').not(e.target).removeClass('in');
-		});
+        // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+        if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+            $('#titleBar, #navPanel, #page-wrapper')
+                .css('transition', 'none');
+        // Initialize WOW.js Scrolling Animations
+        new WOW().init();
+        $('.collapse').on('show.bs.collapse', function (e) {
+            $('.collapse').not(e.target).removeClass('in');
+        });
 
-		$('.smooth').click(function(){
-			$('html, body').animate({
-				scrollTop: $( $.attr(this, 'href') ).offset().top
-			}, 500);
-			return false;
-		});
+        $('.smooth').click(function () {
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 500);
+            return false;
+        });
 
-		$('.wechat').popover({
-			container: '#contact',
-			html: true,
-			trigger: 'hover',
-			placement: 'auto',
-			delay: { "show": 500, "hide": 100 },
-			content: function(){return '<img width="150" height="150" src="'+$(this).data('img') + '" />';}
-		});
+        $('.wechat').popover({
+            container: '#contact',
+            html: true,
+            trigger: 'hover',
+            placement: 'auto',
+            delay: {"show": 500, "hide": 100},
+            content: function () {
+                return '<img width="150" height="150" src="' + $(this).data('img') + '" />';
+            }
+        });
 
-	});
+    });
 })(jQuery);
 
 
