110b975670ad4405da04de76db120535f07dfc37
vvzhangw <vvzhangw@163.com>
2016-06-14 14:49:35 +0800

    1.修改nav；2.增加移动端gototop效果
    1.pc端设置nav最小宽度；2.移动端重新设置nav显示栏；3.只保留nav的border-bottom；4.修改transition效果，加快显示速度；5.增加移动端gototop的显示，并修改速度，使得更快响应

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -17,20 +17,6 @@
 // >>>>>>> origin/iss2
 (function ($) {
 
-    skel
-        .breakpoints({
-            desktop: '(min-width: 737px)',
-            tablet: '(min-width: 737px) and (max-width: 1200px)',
-            mobile: '(max-width: 736px)'
-        })
-        .viewport({
-            breakpoints: {
-                tablet: {
-                    width: 1080
-                }
-            }
-        });
-
     $(function () {
         var $window = $(window),
             $body = $('body');
@@ -43,19 +29,9 @@
         });
 
         // Fix: Placeholder polyfill.
-        $('form').placeholder();
+        // $('form').placeholder();
 
-        // CSS polyfills (IE<9).
-        if (skel.vars.IEVersion < 9)
-            $(':last-child').addClass('last-child');
-
-        // Prioritize "important" elements on mobile.
-        skel.on('+mobile -mobile', function () {
-            $.prioritize(
-                '.important\\28 mobile\\29',
-                skel.breakpoint('mobile').active
-            );
-        });
+        //fixed nav on the top
         var elm = $('#nav');
         var startPos = $(elm).offset().top;
         $.event.add(window, "scroll", function () {
@@ -87,7 +63,7 @@
                 });
             });
             $("#goToTop a").click(function () {
-                $('html,body').animate({scrollTop:0},800);
+                $('html,body').animate({scrollTop:0},400);
                 return false;
             });
         });
@@ -104,40 +80,6 @@
 
         // Off-Canvas Navigation.
 
-        // Title Bar.
-        $(
-            '<div id="titleBar">' +
-            '<a href="#navPanel" class="toggle"></a>' +
-            '<span class="title">' + $('#logo').html() + '</span>' +
-            '</div>'
-        )
-            .appendTo($body);
-
-        //Navigation Panel.
-        $(
-            '<div id="navPanel">' +
-            '<h3>在田信息技术有限公司</h3>' +
-            '<nav>' +
-            $('#nav').navList() +
-            '</nav>' +
-            '</div>'
-        )
-            .appendTo($body)
-            .panel({
-                delay: 500,
-                hideOnClick: true,
-                hideOnSwipe: true,
-                resetScroll: true,
-                resetForms: true,
-                side: 'left',
-                target: $body,
-                visibleClass: 'navPanel-visible'
-            });
-
-        // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-        if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-            $('#titleBar, #navPanel, #page-wrapper')
-                .css('transition', 'none');
         // Initialize WOW.js Scrolling Animations
         new WOW().init();
         $('.collapse').on('show.bs.collapse', function (e) {
