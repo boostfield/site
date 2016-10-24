4574fb86e26f95784b774fc3a513304feb9aa3f8
qky <qky1412@gmail.com>
2016-10-24 09:52:28 +0800

    减小团队成员头像间距；减小合作伙伴图片间距；修改案例对不上号以及显示的逻辑错误

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -32,26 +32,39 @@ jQuery(function($) {
                 case 0:
                     $("#title").hide().text("智能家居").fadeIn();
                     $("#content").hide().text("H5开发").fadeIn();
+                    $wrap.find('#next').show();
+                    $wrap.find('#prev').hide();
                     break;
                 case 1:
                     $("#title").hide().text("聚募-专注早期创业项目的众筹融资平台").fadeIn();
                     $("#content").hide().text("Android APP开发").fadeIn();
+                    $wrap.find('#next').show();
+                    $wrap.find('#prev').show();
                     break;
                 case 2:
                     $("#title").hide().text("音乐圣经-做古典音乐的百科全书").fadeIn();
                     $("#content").hide().text("全平台建设").fadeIn();
+                    $wrap.find('#prev').show();
+                    $wrap.find('#next').show();
                     break;
                 case 3:
-                    $("#title").hide().text("太棒").fadeIn();
-                    $("#content").hide().text("官方网站建设&APP开发").fadeIn();
-                    break;
-                case 4:
                     $("#title").hide().text("厦门大学法学院").fadeIn();
                     $("#content").hide().text("官方网站建设").fadeIn();
+                    $wrap.find('#prev').show();
+                    $wrap.find('#next').show();
+                    break;
+                case 4:
+
+                    $("#title").hide().text("太棒").fadeIn();
+                    $("#content").hide().text("官方网站建设&APP开发").fadeIn();
+                    $wrap.find('#prev').show();
+                    $wrap.find('#next').show();
                     break;
                 case 5:
                     $("#title").hide().text("玩鲜").fadeIn();
                     $("#content").hide().text("iOS&Android开发").fadeIn();
+                    $wrap.find('#prev').show();
+                    $wrap.find('#next').hide();
                     break;
                 default:
                     break;
